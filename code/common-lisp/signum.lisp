06172467db500b3e66eb256dcc482f3914e3f82b
Marco Heisig <marco.heisig@fau.de>
2022-02-10 10:01:40 +0100

    Turn Petalisp's type inference into a separate project.

---
+++ code/common-lisp/signum.lisp
@@ -0,0 +1,15 @@
+(in-package #:typo)
+
+(define-specializer signum (number)
+  (let ((ntype (wrapper-ntype number)))
+    (with-constant-folding (signum (ntype number))
+      (ntype-subtypecase ntype
+        (rational (wrap-default (ntype 'integer)))
+        ((or float (complex float)) (wrap-default ntype))
+        (complex (wrap-default (ntype 'complex)))
+        (t (wrap-default 'number))))))
+
+(define-differentiator signum (number) index
+  (declare (ignore number))
+  ;; Should I care about the discontinuity at zero?  Nah.
+  0)
---
+++ code/differentiate.lisp
@@ -0,0 +1,44 @@
+(in-package #:typo)
+
+(defun differentiate (function
+                      wrappers
+                      wrapper-ntype
+                      wrap-constant
+                      wrap-function
+                      index)
+  (check-type index argument-index)
+  (let ((*wrapper-ntype* wrapper-ntype)
+        (*wrap-constant* wrap-constant)
+        (*wrap-function* wrap-function))
+    (unless (< -1 index (length wrappers))
+      (error 'invalid-differentiation-index
+             :function function
+             :index index
+             :arguments wrappers))
+    (ntype-subtypecase (wrapper-ntype (nth index wrappers))
+      ((not number)
+       (error 'non-numeric-differentiation-argument
+              :function function
+              :index index
+              :arguments wrappers))
+      (t
+       (apply (differentiator function) index wrappers)))))
+
+(defun diff (function arguments n)
+  (flet ((wrap-constant (object)
+           (cons (ntype-of object) object))
+         (wrap-function (ntypes function arguments)
+           (let ((expression (cons function (mapcar #'cdr arguments))))
+             (values-list
+              (loop for ntype in ntypes
+                    collect
+                    (cons ntype expression))))))
+    (let ((result
+            (differentiate
+             function
+             arguments
+             #'first
+             #'wrap-constant
+             #'wrap-function
+             n)))
+      (values (cdr result) (car result)))))
